d4ac7562529dba6df46dce088dfee23865e461b9
sunjing <sunjing@mzhcorp.com>
2022-08-24 01:17:30 +0800

    chore: prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div>  hello,world! </div>
+    <div>
+      {" "}
+      hello,world!
+      <p>i'm coming</p>
+      <p>you are coming</p>
+    </div>
   );
 }
 
